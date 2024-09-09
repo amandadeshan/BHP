e775008be7042d899b05eead3067a441ccf815e1
amandadeshan <140771319+amandadeshan@users.noreply.github.com>
2024-09-09 13:58:59 +0530

    Update and rename BHP.js to office-do.js

---
+++ office-do.js
@@ -0,0 +1,6 @@
+// office-do.js
+function applyOfficeDOCalculation() {
+    // OFFICE/DO-specific OT calculation logic
+    console.log('Applying OFFICE/DO OT calculation');
+    // Example: You might calculate OT based on certain rules here
+}
