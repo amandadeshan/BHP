a5cb2bba9851d8c250bf5ae9d310dbf58b055f90
amandadeshan <140771319+amandadeshan@users.noreply.github.com>
2024-09-09 14:56:40 +0530

    Delete office-do.js

--- office-do.js
+++
@@ -1,6 +0,0 @@
-// office-do.js
-function applyOfficeDOCalculation() {
-    // OFFICE/DO-specific OT calculation logic
-    console.log('Applying OFFICE/DO OT calculation');
-    // Example: You might calculate OT based on certain rules here
-}
