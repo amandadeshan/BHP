92469e8b2bf692155263bb05a75e59000423f432
amandadeshan <140771319+amandadeshan@users.noreply.github.com>
2024-09-09 14:56:32 +0530

    Delete mo.js

--- mo.js
+++
@@ -1,6 +0,0 @@
-// mo.js
-function applyMOCalculation() {
-    // MO-specific OT calculation logic
-    console.log('Applying MO OT calculation');
-    // Example: Implement OT calculation rules specific to MO
-}
